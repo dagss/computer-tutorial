34d835e2bcaf765480cb9a72cf2f3efd80d76d5f
Dag Sverre Seljebotn <dagseljebotn@gmail.com>
2012-09-06 21:06:09 +0200

    Fix pingpong benchmark

--- code/pingpong.c
+++ code/pingpong.c
@@ -5,46 +5,37 @@
 #include "lookatme.h"
 
 #define CACHELINESIZE 64
-#define N (100 * 1000 * 1000)
-#define NSAMPLES 10
+#define N (30 * 1000 * 1000)
 
 int main() {
 
   /* Allocate a memory region *common* to all threads */
-  int32_t *buffer = malloc(CACHELINESIZE * omp_get_max_threads());
+  int8_t *buffer = malloc(CACHELINESIZE * omp_get_max_threads());
 
 #pragma omp parallel firstprivate(buffer)
   {
     int threadnum = omp_get_thread_num();
-    double t_same_line = 1e10;
-    for (int r = 0; r != NSAMPLES; ++r) {
-      double t0 = omp_get_wtime();
-      for (int i = 0; i < N; ++i) {
-        buffer[threadnum] = i;
-        look_at_me(buffer);
-      }
-      double dt = omp_get_wtime() - t0;
-      if (dt < t_same_line) {
-        t_same_line = dt;
-      }
+
+#pragma omp barrier
+    double t0 = omp_get_wtime();
+    for (int i = 0; i < N; ++i) {
+      buffer[threadnum] = i;
+      look_at_me(buffer);
     }
+#pragma omp barrier
+    double t_same_line = omp_get_wtime() - t0;
     if (threadnum == 0) {
       printf("Time taken when sharing cache line: %.2e s\n", t_same_line);
     }
 
 #pragma omp barrier
-    double t_multi_line = 1e10;
-    for (int r = 0; r != NSAMPLES; ++r) {
-      double t0 = omp_get_wtime();
-      for (int i = 0; i < N; ++i) {
-        buffer[threadnum * (CACHELINESIZE / 4)] = i;
-        look_at_me(buffer);
-      }
-      double dt = omp_get_wtime() - t0;
-      if (dt < t_same_line) {
-        t_multi_line = dt;
-      }
+    t0 = omp_get_wtime();
+    for (int i = 0; i < N; ++i) {
+      buffer[threadnum * CACHELINESIZE] = i;
+      look_at_me(buffer);
     }
+    double t_multi_line = omp_get_wtime() - t0;
+#pragma omp barrier
     if (threadnum == 0) {
       printf("Time taken with different cache lines: %.2e s\n", t_multi_line);
       printf("Speedup: %.2f\n", t_same_line / t_multi_line);
